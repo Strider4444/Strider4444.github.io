b9329f3e115ae08e1ca3f34385be2e25c05def89
Strider4444 <aragorn.zulauf@gmail.com>
2026-01-01 18:04:34 +0100

    changed to .png

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const video = document.querySelector("video");
+const map = document.querySelector("img");
 
 setInterval(()=> {
-    video.style.height = window.innerHeight + "px";
-    video.style.width = window.innerWidth + "px";
+    if (window.innerHeight / 1439 * 2559 > window.innerWidth){
+        map.style.width = window.innerWidth + "px";
+        map.style.height = window.innerWidth / 2559 * 1439 + "px";
+    }else {
+        map.style.height = window.innerHeight + "px";
+        map.style.width = window.innerHeight / 1439 * 2559 + "px";
+    }
 }, 100)
