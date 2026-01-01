2b249f4725a67672462a3fe74262688d82900299
Strider4444 <aragorn.zulauf@gmail.com>
2026-01-01 19:42:24 +0100

    added party token

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const map = document.querySelector("img");
+const party = document.querySelector("#party");
 
 setInterval(()=> {
     if (window.innerHeight / 1439 * 2559 > window.innerWidth){
@@ -9,3 +10,9 @@ setInterval(()=> {
         map.style.width = window.innerHeight / 1439 * 2559 + "px";
     }
 }, 100)
+
+party.addEventListener("dragend",(x)=> {
+    console.log(x);
+    party.style.left = x.x - 50 + "px";
+    party.style.top = x.y - 50 + "px";
+})
