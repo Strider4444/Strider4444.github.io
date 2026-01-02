1675857cb248d52810dc6eb98fd6bda94fb69187
Strider4444 <aragorn.zulauf@gmail.com>
2026-01-02 15:53:41 +0100

    added inside of circus

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const map = document.querySelector("video");
 const party = document.querySelector("#party");
+const circusInside = document.querySelector("#circusInside");
 const debug = document.querySelector("#debug");
 
 setInterval(()=> {
@@ -12,6 +13,9 @@ setInterval(()=> {
     }
     party.style.width = map.clientWidth / 40 + "px";
     party.style.height = map.clientWidth / 40 + "px";
+    circusInside.style.width = map.clientWidth / 20 + "px";
+    circusInside.style.left = map.clientWidth / 3.38 + "px";
+    circusInside.style.top = map.clientHeight / 1.74 + "px";
 }, 100)
 
 party.addEventListener("dragend",(x)=> {
@@ -25,4 +29,22 @@ party.addEventListener("touchmove",(x)=> {
     debug.textContent = x.changedTouches[0].pageX + ", " + x.changedTouches[0].pageY + ", " + x.changedTouches;
     party.style.left = x.changedTouches[0].pageX - 50 + "px";
     party.style.top = x.changedTouches[0].pageY - 50 + "px";
+})
+
+circusInside.addEventListener("click",()=> {
+    if (circusInside.style.opacity == 1){
+        circusInside.style.opacity = 0;
+    }else {
+        circusInside.style.opacity = 1;
+    }
+    
+})
+
+circusInside.addEventListener("touchstart",()=> {
+    if (circusInside.style.opacity == 1){
+        circusInside.style.opacity = 0;
+    }else {
+        circusInside.style.opacity = 1;
+    }
+    
 })
